685f6c0d71a76a8944ceb7658b16be0dc225d1ca
Lynda Starkus <45218202+Lynda-Starkus@users.noreply.github.com>
2022-02-25 17:13:01 +0100

    updated frontend challenge

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+
 //
 // Scripts
 // 
