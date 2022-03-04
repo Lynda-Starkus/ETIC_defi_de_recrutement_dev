bb3e28d49011d44a7d01eebaabd19c90d855266d
sones <samyouanes03@gmail.com>
2022-03-04 17:38:22 +0100

    mise à jour
    fini

---
+++ reveal.js
@@ -0,0 +1,19 @@
+
+
+const targets = document.querySelectorAll('.affiche');
+const typing = document.getElementById('ontype');
+function handleIntersection(entries) {
+    entries.map((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible')
+            if (entry.target == typing) {
+                entry.target.classList.add('anime')
+            }
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection);
+
+targets.forEach(target => observer.observe(target));
+
